8062f42b43c3a72171854acd7370c6ca9a0b334e
Gianlu <altomanigianluca@gmail.com>
2021-05-02 16:48:34 +0200

    Use Tremolo decoder only if device is ARM

--- app/src/main/java/xyz/gianlu/librespot/android/LibrespotApp.java
+++ app/src/main/java/xyz/gianlu/librespot/android/LibrespotApp.java
@@ -1,13 +1,28 @@
 package xyz.gianlu.librespot.android;
 
 import android.app.Application;
+import android.os.Build;
+import android.util.Log;
 
 import xyz.gianlu.librespot.audio.decoders.Decoders;
 import xyz.gianlu.librespot.audio.format.SuperAudioFormat;
 import xyz.gianlu.librespot.player.decoders.TremoloVorbisCodec;
 
 public final class LibrespotApp extends Application {
+    private static final String TAG = LibrespotApp.class.getSimpleName();
+
     static {
-        Decoders.replaceDecoder(SuperAudioFormat.VORBIS, TremoloVorbisCodec.class);
+        if (isArm()) {
+            Decoders.replaceDecoder(SuperAudioFormat.VORBIS, TremoloVorbisCodec.class);
+            Log.i(TAG, "Using ARM optimized Vorbis decoder");
+        }
+    }
+
+    private static boolean isArm() {
+        for (String abi : Build.SUPPORTED_ABIS)
+            if (abi.contains("arm"))
+                return true;
+
+        return false;
     }
 }
